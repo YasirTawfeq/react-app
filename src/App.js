bc1290f707c289a8d6c158fb00af48a621def313
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-07-03 22:48:38 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App bg-black">
+        dezfezfezf
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+   <h1 className="bg-red-700">export</h1>
+  </React.StrictMode>
+);
+
