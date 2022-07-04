7bf3a7ff471576d3a344961684cc680c395328e7
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-07-04 16:28:39 +0300

    Adding the top section

---
+++ src/Components/Anime.js
@@ -0,0 +1,11 @@
+import '../index.css'
+function Anime() {
+  return (
+    <div>
+     <h1 className=" mt-10 text-center">Anime</h1>
+
+    </div>
+  )
+}
+
+export default Anime
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Navbar from './Components/Navbar';
+import Anime from './Components/Anime';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-   <h1 className="bg-red-700">export</h1>
+    <Navbar />
+    <Anime />
   </React.StrictMode>
 );
 
