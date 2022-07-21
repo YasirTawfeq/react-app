67931b96943c2f4359d6aa2e9168360e1b2a3950
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-07-21 19:06:57 +0300

    ....

--- src/Components/AnimeHome.js
+++ src/Components/AnimeHome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './Footer'
 function AnimeHome() {
     
     
@@ -7,34 +8,68 @@ function AnimeHome() {
     <div>
      <center className="text-yellow-300">  
        <img className=" w-full lg:w-1/2 h-80" src="https://wallpaper.dog/large/20487367.jpg" alt="" />
-       <div className=" w-full lg:w-1/2 mt-2 lg:ml-3 lg:mr-3 p-3 flex justify-between items-center">
-        <button className="" >
-          <svg className=" bg-yellow-300 w-9 h-9 cursor-pointer rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg>
+       <div className=" w-full lg:w-1/2 mt-2 lg:ml-3 lg:mr-3 p-3 flex justify-between items-center border-b-2 border-yellow-300">
+        <button className="cursor-pointer" >
+          <svg className=" bg-yellow-300 w-9 h-9  rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg>
         </button>
         <p className=" text-xl">Anime Name</p>
        </div>
 
        <div className="w-full lg:w-1/2 mt-3">
         <ul className=" flex justify-center flex-wrap "> 
-         <li className="ml-3">Action</li>
-         <li className="ml-3">Action</li>
+         <li className="ml-3 p-1 text-sm font-bold uppercase text-black bg-yellow-300 rounded-lg ">Action</li>
+         <li className="ml-3 p-1 text-sm font-bold uppercase text-black bg-yellow-300 rounded-lg ">Action</li>
         </ul>
        </div>
       
       <div className="w-full lg:w-1/2 mt-3">
-        <p>lorem ipsum dolor sit amet lorem, consectetur lorem, 
+        <p className="m-1">lorem ipsum dolor sit amet lorem, consectetur lorem, 
         sed diam nonum lorem urnalor lorem ipsum dolor sit amet 
         lorem ipsum dolor sit amet lorem, sed diam nonum lorem 
         ipsum dolor lorem ipsum lorem ipsum dolor</p>
       </div>
 
+      
+     
       <div className="w-full lg:w-1/2 mt-3">
-        <div className="w-full p-4 font-bold uppercase text-black bg-yellow-300 rounded-lg "> Episods </div>
+        <iframe className="w-full aspect-video ..." src="https://www.youtube.com/embed/_sBizpKdDAk"  title="شارة وان بيس One Piece Arabic opening ending" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      
+      <div className="w-full lg:w-1/2 mt-3">
+        <div className="w-full p-2 font-bold uppercase text-black bg-yellow-300 rounded-lg "> Episods </div>
         
       </div>
-       
+
+
+      <div className=" flex flex-wrap justify-evenly w-full lg:w-1/2 mt-3 mb-16">
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+        <div className="font-bold text-black bg-yellow-300 text-xs p-1 rounded-lg mt-1 cursor-pointer active:bg-red-600  ">Episod 1</div>
+      
+      </div>
      </center>
-    
+    <Footer/>
     </div>
   )
 }
